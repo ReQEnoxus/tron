3bd07e6857e143b65948ba5ea8b2b30a84c11633
Enoxus <reqenoxus@gmail.com>
2019-11-30 18:15:28 +0300

    replaced direct scene creation with router calls

--- src/main/java/tron/TronApplication.java
+++ src/main/java/tron/TronApplication.java
@@ -1,20 +1,15 @@
 package tron;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tron.controller.router.Router;
 
 public class TronApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/startMenu.fxml"));
-        primaryStage.setTitle("Tron");
-        Scene menuScene = new Scene(root, 1280, 720);
-        primaryStage.setScene(menuScene);
-
-        primaryStage.show();
+        Router.bind("menu", "/fxml/startMenu.fxml");
+        Router.bind("game", "/fxml/main.fxml");
+        Router.goTo("menu");
     }
 
     public static void main(String[] args) {
