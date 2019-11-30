273562e0671545b0066f4c67c8acf8dbba367bfd
Enoxus <reqenoxus@gmail.com>
2019-11-30 18:10:59 +0300

    removed game related logic from controller

--- src/main/java/tron/controller/MainController.java
+++ src/main/java/tron/controller/MainController.java
@@ -1,20 +1,14 @@
 package tron.controller;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Rectangle;
-import javafx.stage.Stage;
 import tron.model.GameFlow;
 import tron.model.entity.Field;
 
@@ -50,13 +44,7 @@ public class MainController {
 
 
         for (int row = 0; row < noOfCellsInRow; row++) {
-            //ColumnConstraints colConst = new ColumnConstraints(gameGridPane.getMinWidth() / noOfCellsInRow);
-            //gameGridPane.getColumnConstraints().add(colConst);
-
             for (int col = 0; col < noOfCellsInCol; col++) {
-                //RowConstraints rowConst = new RowConstraints(gameGridPane.getMinHeight() / noOfCellsInCol);
-                //gameGridPane.getRowConstraints().add(rowConst);
-
                 Rectangle rect = new Rectangle();
                 rect.setHeight(10);
                 rect.setWidth(10);
@@ -70,31 +58,12 @@ public class MainController {
 
         timeLabel.setText("00:00");
         scoreLabel.setText("Ожидание других игроков");
-    }
 
-    public void startGameLoop() {
-        gameFlow.setBounds(noOfCellsInRow, noOfCellsInCol);
-        gameFlow.setTimeLabel(timeLabel);
-        gameFlow.setScoreLabel(scoreLabel);
-        gameFlow.setSpeedFactor(2);
-
-        gameFlow.startGameLoop();
-    }
-
-    @FXML
-    public void handleKeyPressed(KeyEvent keyEvent) {
-        gameFlow.changePlayerDirection(keyEvent.getCode());
+        GameFlow.getInstance().setScoreLabel(scoreLabel);
+        GameFlow.getInstance().setTimeLabel(timeLabel);
     }
 
     public void exitButtonPressed(ActionEvent actionEvent) throws IOException {
-        // TODO: 24.11.2019 exit from game
-        //gameLoop.stop();
-        Stage stage = ((Stage) ((Button) actionEvent.getSource()).getScene().getWindow());
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/startMenu.fxml"));
-        Parent newScene = loader.load();
-
-        Scene scene = new Scene(newScene);
-        stage.setScene(scene);
-        stage.show();
+        GameFlow.getInstance().stopGame();
     }
 }
