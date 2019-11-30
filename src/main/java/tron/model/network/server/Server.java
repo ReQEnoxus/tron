681e02af823092906634a6e569daba98d5e74b55
Enoxus <reqenoxus@gmail.com>
2019-11-30 18:13:43 +0300

    implemented leaving and ending game functionality

--- src/main/java/tron/model/network/server/Server.java
+++ src/main/java/tron/model/network/server/Server.java
@@ -5,6 +5,7 @@ import tron.model.entity.Player;
 import tron.model.helpers.PlayerHelper;
 import tron.model.network.messages.*;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -17,9 +18,11 @@ public class Server extends Thread {
     private final int PORT = 55555;
     private List<ClientHandler> clients;
     private GameState state;
+    ServerSocket serverSocket;
 
     public Server() {
         clients = new CopyOnWriteArrayList<>();
+        state = new GameState();
         this.start();
     }
 
@@ -29,6 +32,7 @@ public class Server extends Thread {
             try {
                 synchronized (client.out) {
                     client.out.writeObject(introduceClientsMessage);
+                    client.out.flush();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -42,6 +46,7 @@ public class Server extends Thread {
             try {
                 synchronized (client.out) {
                     client.out.writeObject(dataChunk);
+                    client.out.flush();
                 }
             } catch (IOException e) {
                 e.printStackTrace();
@@ -50,6 +55,7 @@ public class Server extends Thread {
     }
 
     private void sendGameStartingMessage() {
+        System.out.println("gameStartingMessageSent");
         GameStartMessage gsm = new GameStartMessage();
         for (ClientHandler client : clients) {
             try {
@@ -70,6 +76,7 @@ public class Server extends Thread {
             synchronized (client.out) {
                 try {
                     client.out.writeObject(roundEndMessage);
+                    client.out.flush();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -83,6 +90,55 @@ public class Server extends Thread {
             synchronized (client.out) {
                 try {
                     client.out.writeObject(roundStartMessage);
+                    client.out.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void sendGameEndMessage(boolean forced) {
+        String message;
+        if (!forced) {
+            message = "Player" + (state.getScores().indexOf(state.getScores().stream().mapToInt(e -> e).max().getAsInt()) + 1) + " победил!";
+        } else {
+            message = "Хост завершил игру";
+        }
+
+        GameEndMessage gameEndMessage = new GameEndMessage(message);
+
+        for (ClientHandler client : clients) {
+            synchronized (client.out) {
+                try {
+                    client.out.writeObject(gameEndMessage);
+                    client.out.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        clients.clear();
+        try {
+            if (!serverSocket.isClosed()) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+    private void sendPlayerLeftMessage(int playerId) {
+        PlayerLeftMessage playerLeftMessage = new PlayerLeftMessage(playerId);
+
+        for (ClientHandler client : clients) {
+            synchronized (client.out) {
+                try {
+                    client.out.writeObject(playerLeftMessage);
+                    client.out.flush();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -91,7 +147,7 @@ public class Server extends Thread {
     }
 
     public void run() {
-        ServerSocket serverSocket = null;
+        serverSocket = null;
 
         try {
             serverSocket = new ServerSocket(PORT);
@@ -102,11 +158,14 @@ public class Server extends Thread {
 
         while (true) {
             try {
-                if (clients.size() < 4) {
+                if (!serverSocket.isClosed()) {
                     new ClientHandler(serverSocket.accept()).start();
+                } else if (serverSocket.isClosed()) {
+                    break;
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                System.out.println("socket has been closed");
+                //e.printStackTrace();
             }
         }
     }
@@ -115,6 +174,7 @@ public class Server extends Thread {
         private Socket clientSocket;
         private ObjectInputStream in;
         private ObjectOutputStream out;
+        private boolean authSuccess = true;
 
         ClientHandler(Socket socket) {
             this.clientSocket = socket;
@@ -126,7 +186,13 @@ public class Server extends Thread {
                 in = new ObjectInputStream(clientSocket.getInputStream());
                 out = new ObjectOutputStream(clientSocket.getOutputStream());
                 out.flush();
-                out.writeObject(new LoginResponse(true, player));
+                if (clients.size() < 4) {
+                    out.writeObject(new LoginResponse(true, player));
+                } else {
+                    out.writeObject(new LoginResponse(false, null));
+                    authSuccess = false;
+                    return;
+                }
                 out.flush();
             } catch (IOException e) {
                 e.printStackTrace();
@@ -143,20 +209,46 @@ public class Server extends Thread {
 
             // Game has started at this point
 
-            state = new GameState();
-
             if (clients.size() == 4) {
+
                 sendGameStartingMessage();
             }
         }
 
         @Override
         public void run() {
+            if (!authSuccess) {
+                return;
+            }
             try {
                 while (true) {
-                    DataChunk dataChunk;
+                    System.out.println(state);
+                    Object serverData;
+                    DataChunk dataChunk = null;
                     synchronized (in) {
-                        dataChunk = ((DataChunk) in.readObject());
+                        try {
+                            serverData = in.readObject();
+                            if (serverData instanceof DataChunk) {
+                                dataChunk = ((DataChunk) serverData);
+                            } else if (serverData instanceof GameExitRequest) {
+                                if (((GameExitRequest) serverData).getPlayerId() != 1) {
+                                    synchronized (out) {
+                                        out.writeObject(new GameExitResponse(((GameExitRequest) serverData).getPlayerId()));
+                                        out.flush();
+                                    }
+                                    clients.remove(this);
+                                    state.getPlayers().remove("PLAYER" + ((GameExitRequest) serverData).getPlayerId());
+                                    state.getPlayersActive().remove("PLAYER" + ((GameExitRequest) serverData).getPlayerId());
+                                    sendPlayerLeftMessage(((GameExitRequest) serverData).getPlayerId());
+                                    break;
+                                } else {
+                                    sendGameEndMessage(true);
+                                    break;
+                                }
+                            }
+                        } catch (EOFException e) {
+                            break;
+                        }
                     }
                     if (dataChunk.isPlayerLost()) {
                         state.getPlayersActive().remove("PLAYER" + dataChunk.getPlayerId());
@@ -173,20 +265,20 @@ public class Server extends Thread {
                         String winner = state.getPlayersActive().get(0);
                         state.endRound(winner);
 
-                        // send everyone round winning message and increment round
-
-
                         sendRoundEndMessage(winner);
 
                         introduceClients();
 
                         System.out.println("Round end message sent");
 
-                        //introduceClients();
-
                         Thread.sleep(2000);
 
-                        sendRoundStartMessage();
+                        if (state.getScores().stream().mapToInt(e -> e).max().getAsInt() == 11) {
+                            sendGameEndMessage(false);
+                            break;
+                        } else {
+                            sendRoundStartMessage();
+                        }
                     }
                 }
 
